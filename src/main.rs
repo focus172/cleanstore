7f5941f7bd95d3db11db644f6d453ca037472a25
Evan Stokdyk <evan.stokdyk@gmail.com>
2023-02-25 00:59:02 -0800

    more

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,113 @@
-use std::path::Path;
+use std::{path::Path, io::{Write, self}};
 use yaml_rust::{YamlLoader, ScanError, Yaml};
 use color_print::cprintln;
 use std::env;
+use no_panic::no_panic;
+
+
 
 const HELP_MESSAGE: &str = "
 cleanstore - a tool to clean up your the files that macOS won't
-Usage: cleanstore [OPTION]
+Usage: cleanstore (OPTION) 
 Options:
-    --help
-    --version
+    --help (-h or help) :  prints this message then exits
+    --version (-v or version) : prints the version of cleanstore then exits
+    --silent (-s) : runs cleanstore in silent mode, no output is printed
+
+";
+//     --daemon (-d) : runs cleanstore as a daemon process, requires root
+
+const DEFAULT_CONFIG_FILE: &str = "
+#
+# cleanstore.yml
+#
+
+silent: false
+
+# leaving blank causes default to home
+root_directory: {}
+
+to_remove: |
+  {}/.lesshst
+  {}/.wget-hsts
+
+directories_to_ignore: |
+  {}/Library
 ";
 
+// [FILE]
+//File:
+//    Alternative root directory to clean up, defaults to the home directory
+
+
+struct DiskSpace {
+    unit: DataType,
+    amount: f32,
+}
+
+impl DiskSpace {
+    pub fn new() -> DiskSpace {
+        DiskSpace {
+            unit: DataType::B,
+            amount: 0.0,
+        }
+    }
+
+    pub fn add(&mut self, amount: u64, unit: DataType) { // taking only this causes overflow at 2GB
+        
+        // this approach is not ideal, but it works to hopefully minimize the amount of overflow
+
+        let altered_ammount = match unit {
+            DataType::B => amount,
+            DataType::KB => amount * 1000,
+            DataType::MB => amount * 1000 * 1000,
+            DataType::GB => amount * 1000 * 1000 * 1000,
+            DataType::TB => amount * 1000 * 1000 * 1000 * 1000,
+        };
+        
+        self.amount += match self.unit {
+            DataType::B => f64::from(altered_ammount as u32),
+            DataType::KB => f64::from(altered_ammount as u32) / (1000.0),
+            DataType::MB => f64::from(altered_ammount as u32) / (1000.0 * 1000.0),
+            DataType::GB => f64::from(altered_ammount as u32) / (1000.0 * 1000.0 * 1000.0),
+            DataType::TB => f64::from(altered_ammount as u32) / (1000.0 * 1000.0 * 1000.0 * 1000.0),
+        } as f32;
+        self.normalize();
+    }
+
+    fn to_string(&self) -> String {
+        match self.unit {
+            DataType::B => format!("{} B", self.amount),
+            DataType::KB => format!("{} KB", self.amount),
+            DataType::MB => format!("{} MB", self.amount),
+            DataType::GB => format!("{} GB", self.amount),
+            DataType::TB => format!("{} TB", self.amount),
+        }
+    }
+
+    fn normalize(&mut self) {
+        if self.amount > 1000.0 {
+            self.amount /= 1000.0;
+            self.unit = match self.unit {
+                DataType::B => DataType::KB,
+                DataType::KB => DataType::MB,
+                DataType::MB => DataType::GB,
+                DataType::GB => DataType::TB,
+                DataType::TB => panic!("cleanstore: overflowed disk space"),
+            };
+        }
+    }
+}
+
+enum DataType {
+    B,
+    KB,
+    MB,
+    GB,
+    TB,
+}
+
+#[no_panic]
 fn main() {
 
     // ------------------------------------------------------------------------------------------------------------
@@ -25,9 +122,13 @@ fn main() {
                 return;
             },
             "--version" | "-v" | "version" => {
-                println!("cleanstore v0.1.0");
+                println!("cleanstore v0.1.1");
                 return;
             },
+            //"--daemon" | "-d" => {
+            //    println!("cleanstore: daemon mode is not yet implemented");
+            //    return;
+            //},
             "--silent" | "-s" => {
                 affirmative_silent = true;
             },
@@ -41,13 +142,18 @@ fn main() {
 
     // ------------------------------------------------------------------------------------------------------------
 
-    let mut amount_removed = 0;
+    let mut amount_removed: DiskSpace = DiskSpace::new();
     let home_dir = std::env::var("HOME").unwrap();
-    let _current_dir = env::current_dir().unwrap();
+    //let _current_dir = env::current_dir().unwrap();
 
     // ------------------------------------------------------------------------------------------------------------
 
-    let config_data: String = std::fs::read_to_string(home_dir.clone() + "/.config/cleanstore/config.yml").unwrap();
+    let config_data_res = read_config_file();
+    let config_data = match config_data_res {
+        Ok(data) => data,
+        Err(_) => panic!("cleanstore: could not read config file"),
+    };
+
     let wrapped_data: Result<Vec<Yaml>, ScanError> = YamlLoader::load_from_str(&config_data);
     let data: Vec<Yaml> = wrapped_data.unwrap(); // TODO: handle better
     let document = &data[0];
@@ -97,7 +203,7 @@ fn main() {
         if file.exists() {
             remove_file(file, &mut amount_removed, &is_silent);
         } else {
-            if !is_silent { cprintln!("<bold>***</bold> <red>File does not exist: {}</red>", file.display()); }
+            //if !is_silent { cprintln!("<bold>***</bold> <red>File does not exist: {}</red>", file.display()); }
         }
     }
 
@@ -112,10 +218,10 @@ fn main() {
 
     // ------------------------------------------------------------------------------------------------------------
 
-    cprintln!("<green>[+]</green> <yellow>Liberated a total of {} bytes!</yellow>", amount_removed);
+    cprintln!("<green>[+]</green> <yellow>Liberated a total of {} bytes!</yellow>", amount_removed.to_string());
 }
 
-fn find_files_rec(current_directory: &Path, amount_removed: &mut u64, directories_to_ignore: &Vec<&Path>, is_silent: &bool) {
+fn find_files_rec(current_directory: &Path, amount_removed: &mut DiskSpace, directories_to_ignore: &Vec<&Path>, is_silent: &bool) {
     for directory_item in std::fs::read_dir(current_directory).unwrap() {
         let item = directory_item.unwrap();
         let path_to_item = item.path();
@@ -130,11 +236,11 @@ fn find_files_rec(current_directory: &Path, amount_removed: &mut u64, directorie
     }
 }
 
-fn remove_file(file_to_remove: &Path, amount_removed: &mut u64, is_silent: &bool) {
+fn remove_file(file_to_remove: &Path, amount_removed: &mut DiskSpace, is_silent: &bool) {
     // get the size of the file and add it to the amount_removed variable
 
     let file_size = file_to_remove.metadata().unwrap().len();
-    *amount_removed += file_size;
+    amount_removed.add(file_size, DataType::B);
 
     if !is_silent { cprintln!("<blue>(-)</blue> Removing file: {} <yellow>({} bytes)</yellow>", file_to_remove.display(), file_size); }
     
@@ -149,4 +255,33 @@ fn should_ignore_file(directories_to_ignore: &Vec<&Path>, file_to_remove: &Path)
     }
 
     return false;
+}
+
+fn read_config_file() -> Result<String, std::io::Error> {
+
+    let home_dir = std::env::var("HOME").unwrap();
+    let data = std::fs::read_to_string(home_dir.clone() + "/.config/cleanstore/config.yml");
+
+    match data {
+        Ok(config_data) => {
+            Ok(config_data)
+        },
+        Err(e) => {
+            // write a new config file after propting the user
+            print!("cleanstore: config file not found, would you like to create a new one? [Y/n] ");
+            let _ = io::stdout().flush(); // need to ensure that the print statement is flushed to the terminal
+            let mut input = String::new();
+            let _ = std::io::stdin().read_line(&mut input);
+            if input.starts_with("y") || input.starts_with("Y") || input.eq("\n") {
+                println!("cleanstore: creating new config file...");
+                let config = DEFAULT_CONFIG_FILE.replace("{}", &home_dir);
+                let mut config_file = std::fs::File::create(home_dir.clone() + "/.config/cleanstore/config.yml").unwrap();
+                let _ = config_file.write_all(config.as_bytes());
+                Ok(config.to_string())
+            } else {
+                println!("cleanstore: exiting...");
+                Err(e)
+            }
+        },
+    }
 }
